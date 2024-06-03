e1536acd2342338c0aadec3539efc77dc7e6e742
AntonioBorsato <antonioborsato16@gmail.com>
2024-06-03 20:32:00 -0300

    Tde04

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        titulo="Timão"
+        texto="Este é o texto do card. Clique para mudar a cor."
+        imagem="https://logodetimes.com/wp-content/uploads/corinthians.png"/>
     </div>
   );
 }
